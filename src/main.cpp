63b266611b646491e62840ddaa6835313616c8d2
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-05-23 00:17:04 -0700

    Front-End/Client - Use dynfu::opencl_depth_device

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <dynfu/kinect_fusion.hpp>
 #include <dynfu/kinect_fusion_opencl_measurement_pipeline_block.hpp>
 #include <dynfu/msrc_file_system_depth_device.hpp>
+#include <dynfu/opencl_depth_device.hpp>
 #include <dynfu/path.hpp>
 #include <dynfu/timer.hpp>
 #include <chrono>
@@ -19,14 +20,16 @@ void main_impl (int, char **) {
 	
 	dynfu::filesystem::path pp(dynfu::current_executable_parent_path());
 	
-	dynfu::msrc_file_system_depth_device_frame_factory ff;
-	dynfu::msrc_file_system_depth_device_filter f;
-	dynfu::file_system_depth_device dd(pp/".."/"data/test/msrc_file_system_depth_device",ff,&f);
-	
 	auto d=boost::compute::system::default_device();
 	boost::compute::context ctx(d);
-	dynfu::file_system_opencl_program_factory opf(pp/".."/"cl",ctx);
 	boost::compute::command_queue q(ctx,d);
+	
+	dynfu::msrc_file_system_depth_device_frame_factory ff;
+	dynfu::msrc_file_system_depth_device_filter f;
+	dynfu::file_system_depth_device ddi(pp/".."/"data/test/msrc_file_system_depth_device",ff,&f);
+	dynfu::opencl_depth_device dd(ddi,q);
+	
+	dynfu::file_system_opencl_program_factory opf(pp/".."/"cl",ctx);
 	dynfu::kinect_fusion_opencl_measurement_pipeline_block mpb(q,opf,20,2.0f,1.0f);
 	
 	dynfu::kinect_fusion kf;
@@ -35,7 +38,7 @@ void main_impl (int, char **) {
 
 	std::size_t total(0);
 	std::size_t frames(0);
-	while (dd) {
+	while (ddi) {
 			
 		dynfu::timer t;
 		kf();
