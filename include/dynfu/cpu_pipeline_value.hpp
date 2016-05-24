a6d4fd168668160fbe96196c60cb8eef19e34e8a
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-05-23 20:49:32 -0700

    dynfu::cpu_pipeline_value::get_or_emplace
    Allows previously constructed values (which may be expensive to construct) to be reused.

--- include/dynfu/cpu_pipeline_value.hpp
+++ include/dynfu/cpu_pipeline_value.hpp
@@ -65,6 +65,36 @@ namespace dynfu {
 				return *obj_;
 				
 			}
+			
+			
+			/**
+			 *	If this object contains a value of type \em T
+			 *	retrieves that value.  Otherwise emplaces an object
+			 *	of type \em T using arguments of type \em Args and
+			 *	returns a reference thereto.
+			 *
+			 *	\tparam Args
+			 *		The types of arguments to forward through to a
+			 *		constructor of \em T if this object does not contain
+			 *		such an object.
+			 *
+			 *	\param [in] args
+			 *		Arguments to types \em Args which shall be forwarded
+			 *		through to a constructor of \em T should it be
+			 *		necessary to construct an object of type \em T.
+			 *
+			 *	\return
+			 *		A reference to the object either created or already
+			 *		contained by this object.
+			 */
+			template <typename... Args>
+			T & get_or_emplace (Args &&... args) noexcept(std::is_nothrow_constructible<T,Args...>::value) {
+				
+				if (!obj_) return emplace(std::forward<Args>(args)...);
+				
+				return *obj_;
+				
+			}
 		
 		
 	};
