9b34ad9207dac7890474d09457a4128833bf089f
Jordan Heemskerk <jordanh@shaw.ca>
2016-05-29 16:35:10 -0700

    dynfu::kinect_fusion_opencl_update_reconstruction_pipeline_block:
     - This has not been verified to produce truly correct TSDF
     - It produces something that looks like a TSDF
     - Unit test to ensure no exceptions are thrown when it is run
     - Closes #66

---
+++ include/dynfu/kinect_fusion_opencl_update_reconstruction_pipeline_block.hpp
@@ -0,0 +1,91 @@
+/**
+ *	\file
+ */
+
+
+#pragma once
+
+
+#include <boost/compute/buffer.hpp>
+#include <boost/compute/command_queue.hpp>
+#include <boost/compute/container/vector.hpp>
+#include <boost/compute/kernel.hpp>
+#include <dynfu/depth_device.hpp>
+#include <dynfu/opencl_program_factory.hpp>
+#include <dynfu/opencl_vector_pipeline_value.hpp>
+#include <dynfu/optional.hpp>
+#include <dynfu/update_reconstruction_pipeline_block.hpp>
+#include <Eigen/Dense>
+#include <cstddef>
+
+
+
+
+namespace dynfu {
+	
+	
+	/**
+	 *	An \ref update_reconstruction_pipeline_block implemented
+	 *	as per the Kinect Fusion paper.
+	 *
+	 *	\sa kinect_fusion
+	 */
+	class kinect_fusion_opencl_update_reconstruction_pipeline_block : public update_reconstruction_pipeline_block {
+		
+		
+		private:
+			opencl_vector_pipeline_value_extractor<float> ve_;
+			boost::compute::kernel tsdf_kernel_;
+			boost::compute::buffer t_g_k_vec_buf_;
+			boost::compute::buffer ik_buf_;
+			boost::compute::buffer k_buf_;
+			boost::compute::buffer proj_view_buf_;
+			optional<Eigen::Matrix4f> t_g_k_;
+			optional<Eigen::Matrix3f> k_;
+			float mu_;
+			
+			std::size_t tsdf_width_;
+			std::size_t tsdf_height_;
+			std::size_t tsdf_depth_;
+			
+		
+		public:
+			
+			kinect_fusion_opencl_update_reconstruction_pipeline_block () = delete;
+			
+			/**
+			 *	Creates a new kinect_fusion_opencl_update_reconstruction_pipeline_block.
+			 *
+			 *	\param [in] q
+			 *		A boost::compute::command_queue which the newly created
+			 *		object shall use to dispatch OpenCL tasks.
+			 *	\param [in] opf
+			 *		An \ref opencl_program_factory which the newly created
+			 *		object shall use to obtain OpenCL programs.
+			 *	\param [in] mu
+			 *		\f$\mu\f$: the truncation distance of the truncated signed distance function (TSDF).
+			 *		Points greater than a distance of \f$\mu\f$ from the nearest surface interface are
+			 *		truncated to the maximum distance of \f$\mu\f$. Non-visible points farther than \f$\mu\f$
+			 *		from the surface are not measured.
+			 *	\param [in] tsdf_width
+			 *		The width of the TSDF.
+			 *	\param [in] tsdf_height
+			 *		The height of the TSDF.
+			 *	\param [in] tsdf_depth
+			 *		The depth of the TSDF.
+			 */
+			kinect_fusion_opencl_update_reconstruction_pipeline_block (				
+				boost::compute::command_queue q,
+				opencl_program_factory & opf,
+				float mu,
+				std::size_t tsdf_width=512,
+				std::size_t tsdf_height=512,
+				std::size_t tsdf_depth=512	
+			);
+			
+			virtual value_type operator () (depth_device::value_type::element_type & frame, std::size_t width, std::size_t height, Eigen::Matrix3f k, Eigen::Matrix4f T_g_k, value_type v=value_type{});
+			 
+	};
+	
+	
+}
--- include/dynfu/update_reconstruction_pipeline_block.hpp
+++ include/dynfu/update_reconstruction_pipeline_block.hpp
@@ -5,6 +5,11 @@
 
 #pragma once
 
+#include <dynfu/depth_device.hpp>
+#include <dynfu/pipeline_value.hpp>
+#include <cstddef>
+#include <memory>
+#include <vector>
 
 namespace dynfu {
 	
@@ -23,6 +28,44 @@ namespace dynfu {
 		
 		public:
 		
+			/**
+			 *	A value in the TSDF.
+			 *
+			 *	Each voxel in the tsdf has an associated value, forming a scalar
+			 *	field in \f${\rm I\!R^3}\f$.
+			 */
+			using buffer_type=std::vector<float>;
+			
+		
+			/**
+			 *	Represents the TSDF.
+			 */
+			class value_type {
+				
+				public:
+					
+					/**
+					 *	A std::unique_ptr to a pipeline_value that stores the buffer of the TSDF.
+					 */
+					std::unique_ptr<pipeline_value<buffer_type>> buffer;
+					
+					/**
+					 * The width of the TSDF.
+					 */
+					std::size_t width;
+					
+					/**
+					 *	The height of the TSDF.
+					 */
+					std::size_t height;
+					
+					/**
+					 *	The depth of the TSDF.
+					 */
+					std::size_t depth;
+				
+			};
+	
 		
 			update_reconstruction_pipeline_block () = default;
 			update_reconstruction_pipeline_block (const update_reconstruction_pipeline_block &) = delete;
@@ -36,7 +79,35 @@ namespace dynfu {
 			 *	through pointer or reference to base.
 			 */
 			virtual ~update_reconstruction_pipeline_block () noexcept;
-		
+
+
+			/**
+			 *	Updates the global reconstruction of the scene with the
+			 *	current raw depth frame data and returns the local TSDF.
+			 *
+			 *	\param [in] frame
+			 *		A \ref pipeline_value representing the depth
+			 *		frame.
+			 *	\param [in] width
+			 *		The width of \em frame.
+			 *	\param [in] height
+			 *		The height of \em frame.
+			 *	\param [in] k
+			 *		The \f$K\f$ matrix for the \ref depth_device
+			 *		which generated \em frame.
+			 *	\param [in] T_g_k
+			 *		The \f$T_{g,k}\f$ matrix for the current frame, \f$k\f$.
+			 *	\param [in] v
+			 *		A std::unique_ptr to a \ref pipeline_value which
+			 *		this object may use rather than allocating a new
+			 *		one.  Defaults to a default constructed std::unique_ptr
+			 *		which will cause this object to allocate a new
+			 *		object to return.
+			 *
+			 *	\return
+			 *		A \ref pipeline_value representing the TSDF generated from \em frame.
+			 */
+			virtual value_type operator () (depth_device::value_type::element_type & frame, std::size_t width, std::size_t height, Eigen::Matrix3f k, Eigen::Matrix4f T_g_k, value_type v=value_type{}) = 0;
 		
 	};
 	
