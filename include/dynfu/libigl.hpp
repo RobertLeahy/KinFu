486cb9e8f8ca83b8d112e8d48759dec2e169b545
Jordan Heemskerk <jordanh@shaw.ca>
2016-07-18 11:15:47 -0700

    client: Add ability to read/write .OFF files

--- include/dynfu/libigl.hpp
+++ include/dynfu/libigl.hpp
@@ -4,7 +4,7 @@
 
 
 #include <Eigen/Dense>
-
+#include <string>
 
 namespace dynfu {
 
@@ -16,6 +16,12 @@ namespace dynfu {
 
 
 		void marching_cubes (const Eigen::VectorXd & scalar_field, const Eigen::MatrixXd & field_Points, std::size_t x, std::size_t y, std::size_t z, Eigen::MatrixXd & vertices, Eigen::MatrixXi & faces);
+
+
+		void writeOFF(const std::string path, Eigen::MatrixXd & vertices, Eigen::MatrixXi & faces);
+
+
+		void readOFF(const std::string path, Eigen::MatrixXd & vertices, Eigen::MatrixXi & faces);
 
 
 	}
--- src/libigl.cpp
+++ src/libigl.cpp
@@ -1,6 +1,8 @@
 #include <dynfu/libigl.hpp>
 #include <igl/copyleft/marching_cubes.h>
+#include <igl/readOFF.h>
 #include <igl/viewer/Viewer.h>
+#include <igl/writeOFF.h>
 
 
 namespace dynfu {
@@ -21,6 +23,20 @@ namespace dynfu {
 		void marching_cubes (const Eigen::VectorXd & scalar_field, const Eigen::MatrixXd & field_points, std::size_t x, std::size_t y, std::size_t z, Eigen::MatrixXd & vertices, Eigen::MatrixXi & faces) {
 
 			igl::copyleft::marching_cubes(scalar_field,field_points,x,y,z,vertices,faces);
+
+		}
+
+
+		void writeOFF(const std::string path, Eigen::MatrixXd & vertices, Eigen::MatrixXi & faces) {
+		
+			igl::writeOFF(path,vertices,faces);
+
+		}
+
+
+		void readOFF(const std::string path, Eigen::MatrixXd & vertices, Eigen::MatrixXi & faces) {
+		
+			igl::readOFF(path,vertices,faces);
 
 		}
 
