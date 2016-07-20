a32e25d345b392fb2b138f03915eb8f11b4ca20f
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-07-20 00:00:08 -0700

    dynfu::to_pixel & dynfu::to_camera

---
+++ src/test/camera.cpp
@@ -0,0 +1,57 @@
+#include <dynfu/camera.hpp>
+
+
+#include <Eigen/Dense>
+#include <cmath>
+#include <catch.hpp>
+
+
+SCENARIO("dynfu::to_pixel translates from camera to pixel coordinates and dynfu::to_camera translates from pixel to camera coordinates","[dynfu][to_pixel][to_camera]") {
+
+	GIVEN("A camera calibration matrix, a pixel, and a depth") {
+
+		Eigen::Matrix3f k;
+		k <<	585.0f, 0.0f, 320.0f,
+				0.0f, -585.0f, 240.0f,
+				0.0f, 0.0f, 1.0f;
+		
+		Eigen::Vector2i pixel(0,0);
+		float depth(1.2f);
+
+		WHEN("dynfu::to_camera is invoked thereupon") {
+
+			auto cam=dynfu::to_camera(pixel,depth,k);
+
+			THEN("The result is correct") {
+
+				CHECK(cam(2)==Approx(1.2f));
+				CHECK(cam(0)<0.0f);
+				CHECK(cam(1)>0.0f);
+				CHECK(std::abs(cam(0))>std::abs(cam(1)));
+
+				AND_WHEN("The result is passed to dynfu::to_pixel") {
+
+					auto pair=dynfu::to_pixel(cam,k);
+
+					THEN("The original pixel is retrieved") {
+
+						CHECK(pair.first(0)==pixel(0));
+						CHECK(pair.first(1)==pixel(1));
+
+					}
+
+					THEN("The original depth is retrieved") {
+
+						CHECK(pair.second==Approx(depth));
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
